f3b55d2ce2311030ac3fd6f63431001b2f3b1ad1
ChanghyeonKim93 <hyun91015@gmail.com>
2020-09-19 02:58:29 +0900

    triggered node for Camlidar module

---
+++ include/bluefox_multiple_ros_camlidar.h
@@ -0,0 +1,174 @@
+#ifndef _BLUEFOX_MULTIPLE_ROS_CAMLIDAR_H_
+#define _BLUEFOX_MULTIPLE_ROS_CAMLIDAR_H_
+
+#include <iostream>
+#include <vector>
+#include <sys/time.h>
+
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+
+#include <Eigen/Dense>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <apps/Common/exampleHelper.h>
+#include <mvIMPACT_CPP/mvIMPACT_acquire.h>
+
+#include "bluefox.h"
+
+#include "dynamic_reconfigure/server.h"
+#include "bluefox/bluefoxDynConfig.h"
+
+#include <camlidar_module/trg_msg.h> // custom message
+
+using namespace std;
+using namespace mvIMPACT::acquire;
+
+class BlueFOX_MULTIPLE_ROS_CAMLIDAR {
+public:
+    explicit BlueFOX_MULTIPLE_ROS_CAMLIDAR(
+        ros::NodeHandle& nh, bool binning_on, bool triggered_on,
+        bool aec_on, bool agc_on, bool hdr_on, int expose_us, double frame_rate)
+    : nh_(nh), it_(nh_)
+    {
+        n_devs_ = getValidDevices(devMgr_, validDevices_);
+        std::cout << "# of valid devices: " << n_devs_ << std::endl;
+        // show devices information
+        for(int i = 0; i < n_devs_; i++){
+            std::cout << "[" << i << "]: ";
+            BlueFox* bluefox_temp = 
+                        new BlueFox(validDevices_[i], i, binning_on, triggered_on, 
+                        aec_on, agc_on, hdr_on, expose_us, frame_rate);
+            std::string topic_name = "/" + std::to_string(i) + "/image_raw";
+
+            bluefoxs_.push_back(bluefox_temp);
+
+            image_transport::Publisher camera_pub_ = it_.advertise(topic_name,1);
+            image_publishers_.push_back(camera_pub_);
+            img_msgs_.push_back(sensor_msgs::Image());
+        }
+	cout << " mcu trigger subscriber.\n";
+        sub_msg_ = nh_.subscribe("/mcu/trigger", 1, &BlueFOX_MULTIPLE_ROS_CAMLIDAR::callback, this);
+        cout << "Please wait for setting cameras...\n";
+        ros::Duration(1.0).sleep();
+        cout << "camera setting is done.\n";
+    };    
+    ~BlueFOX_MULTIPLE_ROS_CAMLIDAR();
+
+    void callback(const camlidar_module::trg_msgConstPtr& msg);
+    void callbackDynReconfig(bluefox::bluefoxDynConfig &config, uint32_t lvl);
+
+private:
+    int n_devs_; // # of connected mvBlueCOUGAR cameras.
+    mvIMPACT::acquire::DeviceManager devMgr_; // Manager for all devices.
+
+    vector<mvIMPACT::acquire::Device*> validDevices_; // multiple devices
+    vector<BlueFox*> bluefoxs_;
+    
+    
+    // For ros.
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+
+    vector<image_transport::Publisher> image_publishers_;
+    vector<sensor_msgs::Image> img_msgs_;
+
+    ros::Subscriber sub_msg_;
+
+    dynamic_reconfigure::Server<bluefox::bluefoxDynConfig> server;
+    dynamic_reconfigure::Server<bluefox::bluefoxDynConfig>::CallbackType f;
+};
+
+/* IMPLEMENTATION */
+BlueFOX_MULTIPLE_ROS_CAMLIDAR::~BlueFOX_MULTIPLE_ROS_CAMLIDAR(){
+    for(int i = 0; i < n_devs_; i++){
+        delete bluefoxs_[i];
+    }
+};
+
+void BlueFOX_MULTIPLE_ROS_CAMLIDAR::callback(const camlidar_module::trg_msgConstPtr& msg)
+{
+    std::cout << "CAMNODE time: "<< msg->stamp.sec<<"."<<msg->stamp.nsec << "\n";
+    bool state_grab = true;
+    // snapshot grab mode.
+    for(int i = 0; i < n_devs_; i++){
+    	state_grab = state_grab & bluefoxs_[i]->grabImage(img_msgs_[i]);
+    }   
+    if(state_grab){
+    	for(int i = 0; i <n_devs_; i++){
+    	    image_publishers_[i].publish(img_msgs_[i]);
+        }
+    }
+};
+
+void BlueFOX_MULTIPLE_ROS_CAMLIDAR::callbackDynReconfig(bluefox::bluefoxDynConfig &config, uint32_t lvl) {
+    if(config.hdr){
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setHighDynamicRange(true);
+        cout << " DYNRECONFIG HDR:" <<  config.hdr << "\n";
+    }
+    else{
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setHighDynamicRange(false);
+    }
+        
+    if(config.trigger_mode){
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setTriggerMode(true);
+    }
+    else{
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setTriggerMode(false);
+    }
+    
+    if(config.binning_mode){
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setBinningMode(true);
+    }
+    else{
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setBinningMode(false);
+    }
+
+    if(config.aec){
+        for(int i = 0; i < n_devs_; i++){
+            bluefoxs_[i]->setAutoExposureMode(true);
+        }
+    }
+    else{
+        for(int i = 0; i < n_devs_; i++){
+            bluefoxs_[i]->setAutoExposureMode(false);
+            bluefoxs_[i]->setExposureTime(config.expose_us);
+        }
+    }
+
+    if(config.agc){
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setAutoGainMode(true);
+    }
+    else{
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setAutoGainMode(false);
+    }
+
+    if(config.wbp == -1){ // off
+       for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setWhiteBalance(config.wbp,0,0,0);
+    }
+    else{ // on! each mode...
+        for(int i = 0; i < n_devs_; i++)
+            bluefoxs_[i]->setWhiteBalance(config.wbp,0,0,0);
+    }
+    
+    ROS_INFO("Parameter reconfigured.\n");
+};
+#endif
---
+++ src/main_camlidar.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <ros/ros.h>
+
+#include "bluefox_multiple_ros_camlidar.h"
+#include "dynamic_reconfigure/server.h"
+#include "bluefox/bluefoxDynConfig.h"
+
+
+// trigger pin = digIn0+
+// trigger ground level is same with the Arduino ground level.
+// TODO: recognize serial numbers of multiple cameras and grant specific "id".
+// TODO: multiple cameras setting is not supported yet .. T.T
+int main(int argc, char **argv) {
+    std::cout << "BlueFOX node is running..." << std::endl;
+    ros::init(argc, argv, "bluefox_multiple_node");
+
+    ros::NodeHandle nh("~");
+
+    bool binning_on   = false;
+    bool triggered_on = true;
+    bool aec_on       = false; // auto exposure control on / off
+    bool agc_on       = false; // auto gain control on / off
+    bool hdr_on       = false;
+    int expose_us     = 11000; // it is also max. exposure for auto exposure control.
+    double frame_rate = 60.0; // frame rate (full resolution: up to 30 Hz)
+
+    ros::param::get("~binning_on", binning_on);
+    ros::param::get("~triggered_on", triggered_on);
+    ros::param::get("~aec_on", aec_on);
+    ros::param::get("~agc_on", agc_on);
+    ros::param::get("~hdr_on", hdr_on);
+    ros::param::get("~expose_us", expose_us);
+    ros::param::get("~frame_rate", frame_rate);
+
+    BlueFOX_MULTIPLE_ROS_CAMLIDAR *bluefoxs = 
+        new BlueFOX_MULTIPLE_ROS_CAMLIDAR(nh, binning_on, triggered_on, 
+        aec_on, agc_on, hdr_on, expose_us, frame_rate);
+    
+    while(ros::ok()) {
+        ros::spinOnce();
+    }
+
+    delete bluefoxs;
+    ROS_INFO_STREAM("Cease cameras.\n");
+    return -1;
+};
